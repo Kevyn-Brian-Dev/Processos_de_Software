f1a59fd67aa201c7debea82eab9c5fee8da16292
kevyn Brian <=>
2025-06-19 07:35:21 -0300

    Finalizção html/css

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+const generatePasswordButton = document.querySelector("#generate_password");
+const generatedPasswordButton = document.querySelector("#generated_password");
+
+
+generatePasswordButton.addEventListener("click", () => {
+
+    console.log("teste")
+})
