6e1376021c8985199d09e0ce77b96e41122f02ea
kevyn Brian <=>
2025-06-24 15:26:31 -0300

    finalização dos CheckBox

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,10 @@
 const generatePasswordButton = document.querySelector("#generate-password");
 const generatedPasswordElement = document.querySelector("#generated-password");
+const includeNumbersCheckbox = document.querySelector("#include-numbers");
+const includeSymbolsCheckbox = document.querySelector("#include-symbols");
+const includeLowercaseCheckbox = document.querySelector("#include-lowercase");
+const includeUppercaseCheckbox = document.querySelector("#include-uppercase");
+
 
 const getLetterLowerCase = () => {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
@@ -11,31 +16,54 @@ const getLetterUpperCase = () => {
 
 const getNumber = () => {
     return Math.floor(Math.random() * 10).toString();
-}
+};
 
 const getSymbol = () => {
     const symbols = "!@#$%^&*()_+-={}[]:;'<>?/";
     return symbols[Math.floor(Math.random() * symbols.length)];
-}
+};
 
-const generatePassword = (getLetterLowerCase, getLetterUpperCase, getNumber, getSymbol) => {
-    let password = ""
-    const passwordLength = 10
-    const generators = [getLetterLowerCase, getLetterUpperCase, getNumber, getSymbol]
+const generatePassword = () => {
+    let password = "";
+    const passwordLength = 10;
 
-    for(let i = 0; i < passwordLength; i = i + 4) {
-        generators.forEach(() => {
-            const randomValue = generators[Math.floor(Math.random() * generators.length)]();
+    const generators = [];
 
-            password += randomValue;
-        });
+    if (includeLowercaseCheckbox.checked) {
+        generators.push(getLetterLowerCase);
+    }
+    if (includeUppercaseCheckbox.checked) {
+        generators.push(getLetterUpperCase);
+    }
+    if (includeNumbersCheckbox.checked) {
+        generators.push(getNumber);
+    }
+    if (includeSymbolsCheckbox.checked) {
+        generators.push(getSymbol);
     }
 
+    if (generators.length === 0) {
+        alert("Por favor, selecione pelo menos uma opção! para gerar uma senha.");
+        generatedPasswordElement.style.display = "none";
+        if (copyPasswordIcon) copyPasswordIcon.style.display = "none"; 
+        return;
+    }
+
+    for (let i = 0; i < passwordLength; i++) {
+        const randomGenerator = generators[Math.floor(Math.random() * generators.length)];
+        password += randomGenerator();
+    }
+
+    password = password.split('').sort(() => Math.random() - 0.5).join('');
+
     password = password.slice(0, passwordLength);
+
     generatedPasswordElement.style.display = "block";
     generatedPasswordElement.querySelector("h4").innerText = password;
+
+    document.querySelector("#password").value = password;
 };
 
 generatePasswordButton.addEventListener("click", () => {
-    generatePassword(getLetterLowerCase, getLetterUpperCase, getNumber, getSymbol);
+    generatePassword();
 });
